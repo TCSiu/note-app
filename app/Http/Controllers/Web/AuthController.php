74762e2758d9d03d424a94f572adbb4fd2527a27
TCSiu <amble3232@gmail.com>
2023-12-03 16:29:59 +0800

    feat: init

---
+++ app/Http/Controllers/Web/AuthController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Web;
+
+use App\Http\Controllers\Web\BaseController;
+use Illuminate\Http\Request;
+
+class AuthController extends BaseController
+{
+    public function login(Request $request){
+
+    }
+
+    public function register(Request $request){
+        
+    }
+}
