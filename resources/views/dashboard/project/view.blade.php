eb86d2ef507877119cd0d12a26dcefcb6b4e03e9
TCSiu <amble3232@gmail.com>
2023-12-07 18:17:46 +0800

    feat: 2023-07-12

--- resources/views/dashboard/project/view.blade.php
+++ resources/views/dashboard/project/view.blade.php
@@ -2,25 +2,41 @@
 
 @section('content')
 <main class="content">
-	<div class="container-fluid p-0">
-		<h1 class="h3 mb-3">{{ __('Content Management System') }}</h1>
-		<div class="row justify-content-center" data-masonry='{"percentPosition": true }'>
+    <div class="container-fluid p-0">
+        <h1 class="h3 mb-3">{{ __('Content Management System') }}</h1>
+        <div class="row justify-content-center" data-masonry='{"percentPosition": true }'>
             @isset($tasks)
-            @foreach($tasks as $k =>  task)
+            @foreach($tasks as $k => $task)
             <div class="col-12 col-sm-6 col-lg-4 mb-4">
                 <div class="card">
-
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $task->name ?? '' }}</h5>
+                        <p class="card-text">{{ $task->description ?? '' }}</p>
+                        <button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">Toggle right offcanvas</button>
+                    </div>
                 </div>
             </div>
             @endforeach
             @endif
         </div>
-	</div>
+    </div>
 </main>
+
+<div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
+    <div class="offcanvas-header">
+      <h5 id="offcanvasRightLabel">Offcanvas right</h5>
+      <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+    </div>
+    <div class="offcanvas-body">
+      ...
+    </div>
+  </div>
 @stop
 
 @push('scripts')
-<script src="https://cdn.jsdelivr.net/npm/masonry-layout@4.2.2/dist/masonry.pkgd.min.js" integrity="sha384-GNFwBvfVxBkLMJpYMOABq3c+d3KnQxudP/mGPkzpZSTYykLBNsZEnG2D9G/X/+7D" crossorigin="anonymous" async></script>
+<script src="https://cdn.jsdelivr.net/npm/masonry-layout@4.2.2/dist/masonry.pkgd.min.js"
+    integrity="sha384-GNFwBvfVxBkLMJpYMOABq3c+d3KnQxudP/mGPkzpZSTYykLBNsZEnG2D9G/X/+7D" crossorigin="anonymous"
+    async></script>
 <script>
 
 </script>
